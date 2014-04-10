719116a1d132a72f3bd2b9771192c4de014e5e34
Jake Prather <JakeHP@Zoho.com>
2014-04-10 10:21:44 -0500

    Monitor which errors have been seen and clear in routing

--- client/views/includes/error.js
+++ client/views/includes/error.js
@@ -3,3 +3,9 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.defer(function() { //in case of rerending of page, only run after that happens
+    Errors.update(error._id, {$set: {seen: true}});
+  });
+};
