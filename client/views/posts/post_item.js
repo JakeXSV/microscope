6f1138f741e3a3f53d74447c3ebc13a722130c08
Jake Prather <JakeHP@Zoho.com>
2014-04-12 19:38:19 -0500

    grey out upvote button when not logged in / already voted

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -9,11 +9,19 @@ Template.postItem.helpers({
   },
   commentsCount: function() {
     return this.commentsCount;
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)){
+      return 'btn-primary upvotable';
+    }else{
+      return 'disabled';
+    }
   }
 });
 
 Template.postItem.events({
-  'click .upvote': function(e) {
+  'click .upvotable': function(e) {
     e.preventDefault();
     Meteor.call('upvote', this._id);
   }
